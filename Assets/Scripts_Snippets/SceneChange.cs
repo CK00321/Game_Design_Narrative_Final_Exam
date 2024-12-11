1849006f8b2a764889ade5174153108dba17bde5
CK00321 <200600276@student.georgianc.on.ca>
2024-12-10 23:55:58 -0500

    FINAL

--- Assets/Scripts_Snippets/SceneChange.cs
+++ Assets/Scripts_Snippets/SceneChange.cs
@@ -6,10 +6,32 @@ using Yarn.Unity;
 
 public class SceneChange : MonoBehaviour
 {
-    [YarnCommand("ChangeMyScene")]
-    // Start is called before the first frame update
-    public void ChangeMyScene()
+
+    void Start()
+    {
+
+    }
+    [YarnCommand("MyScene2")]
+    public void MyScene2()
     {
         SceneManager.LoadScene("Explore_Mansion");
     }
+
+    [YarnCommand("MyScene3")]
+    public void MyScene3()
+    {
+        SceneManager.LoadScene("The_Canvas_&_The_Entity");
+    }
+
+    [YarnCommand("MyScene4")]
+    public void MyScene4()
+    {
+        SceneManager.LoadScene("Ending_1_The_Cursed_Canvas");
+    }
+    [YarnCommand("MyScene5")]
+    public void MyScene5()
+    {
+        SceneManager.LoadScene("Ending_2_The_Cursed_Canvas");
+    }
+
 }
