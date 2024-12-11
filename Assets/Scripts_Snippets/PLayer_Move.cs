f6ab1b23de9198bf92c24002b8819544403955c9
CK00321 <200600276@student.georgianc.on.ca>
2024-12-10 22:56:35 -0500

    Major change

--- Assets/Scripts_Snippets/PLayer_Move.cs
+++ Assets/Scripts_Snippets/PLayer_Move.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed = 5.0f; 
+    public float Speed = 2.50f;
 
-    void Update()
+    public GameObject TextE;
+
+    void FixedUpdate()
     {
 
         float moveX = Input.GetAxis("Horizontal");
@@ -16,5 +18,21 @@ public class PlayerMovement : MonoBehaviour
         Player = Player.normalized * Speed * Time.deltaTime;
 
         transform.Translate(Player);
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Mimushi") || collision.gameObject.CompareTag("Mitushi")) 
+        {
+            Debug.Log("Enter_The_Dialogue");
+            TextE.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Dialogue_System_Is_Unreachable");
+        TextE.SetActive(false);
     }
 }
